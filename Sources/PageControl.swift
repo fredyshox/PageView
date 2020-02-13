e7fb1bb310f68894f10f4b0cf46afbf45993fb7c
Kacper Raczy <gfw.kra@gmail.com>
2020-02-14 00:24:54 +0100

    Replaces page control body protocol with closures

--- Sources/PageControl.swift
+++ Sources/PageControl.swift
@@ -7,38 +7,6 @@
 
 import SwiftUI
 
-public protocol PageControlBody: View {
-    init(pageCount: Int, theme: PageControlTheme, selectedPage: Int)
-}
-
-public struct DefaultPageControlBody: View, PageControlBody {
-    public let pageCount: Int
-    public let theme: PageControlTheme
-    public let selectedPage: Int
-    
-    public init(pageCount: Int, theme: PageControlTheme, selectedPage: Int) {
-        self.pageCount = pageCount
-        self.theme = theme
-        self.selectedPage = selectedPage
-    }
-    
-    public var body: some View {
-        ForEach(0..<pageCount) { (i) in
-             Circle()
-                 .frame(width: self.theme.dotSize, height: self.theme.dotSize)
-                 .foregroundColor(self.dotColor(index: i))
-         }
-    }
-    
-    private func dotColor(index: Int) -> Color {
-        if index == selectedPage {
-            return theme.dotActiveColor
-        } else {
-            return theme.dotInactiveColor
-        }
-    }
-}
-
 public enum PageControl {
     struct Background: ViewModifier {
         let theme: PageControlTheme
@@ -56,38 +24,77 @@ public enum PageControl {
         }
     }
     
-    public struct Horizontal<Body>: View where Body: PageControlBody {
+    public struct Horizontal<Body>: View where Body: View {
+        public typealias PageControlBody = (Int, Int, PageControlTheme) -> Body
+        
         public let pageCount: Int
-        public let theme: PageControlTheme
         public let selectedPage: Int
+        public let theme: PageControlTheme
+        public let bodyCreator: PageControlBody
 
         public var body: some View {
             HStack(spacing: theme.spacing) {
-                Body(pageCount: pageCount, theme: theme, selectedPage: selectedPage)
+                bodyCreator(pageCount, selectedPage, theme)
             }.modifier(Background(theme: theme))
         }
     }
 
-    public struct Vertical<Body>: View where Body: PageControlBody {
+    public struct Vertical<Body>: View where Body: View {
+        public typealias PageControlBody = (Int, Int, PageControlTheme) -> Body
+        
         public let pageCount: Int
-        public let theme: PageControlTheme
         public let selectedPage: Int
+        public let theme: PageControlTheme
+        public let bodyCreator: PageControlBody
 
         public var body: some View {
             VStack(spacing: theme.spacing) {
-                Body(pageCount: pageCount, theme: theme, selectedPage: selectedPage)
+                bodyCreator(pageCount, selectedPage, theme)
             }.modifier(Background(theme: theme))
         }
     }
     
-    typealias DefaultHorizontal = Horizontal<DefaultPageControlBody>
-    typealias DefaultVertical = Vertical<DefaultPageControlBody>
+    // MARK: Default implementation
+    
+    public struct DefaultPageControlBody: View {
+        public let pageCount: Int
+        public let selectedPage: Int
+        public let theme: PageControlTheme
+        
+        public var body: some View {
+            ForEach(0..<pageCount) { (i) in
+                 Circle()
+                     .frame(width: self.theme.dotSize, height: self.theme.dotSize)
+                     .foregroundColor(self.dotColor(index: i))
+             }
+        }
+        
+        private func dotColor(index: Int) -> Color {
+            if index == selectedPage {
+                return theme.dotActiveColor
+            } else {
+                return theme.dotInactiveColor
+            }
+        }
+    }
+    
+    public static func DefaultHorizontal(pageCount: Int, selectedPage: Int, theme: PageControlTheme) -> Horizontal<DefaultPageControlBody> {
+        return Horizontal(pageCount: pageCount, selectedPage: selectedPage, theme: theme) {
+            DefaultPageControlBody(pageCount: $0, selectedPage: $1, theme: $2)
+        }
+    }
+    
+    public static func DefaultVertical(pageCount: Int, selectedPage: Int, theme: PageControlTheme) -> Vertical<DefaultPageControlBody> {
+        return Vertical(pageCount: pageCount, selectedPage: selectedPage, theme: theme) {
+            DefaultPageControlBody(pageCount: $0, selectedPage: $1, theme: $2)
+        }
+    }
 }
 
 #if DEBUG
 struct PageControl_Previews: PreviewProvider {
     static var previews: some View {
-        PageControl.DefaultHorizontal(pageCount: 3, theme: .default, selectedPage: 0)
+        PageControl.DefaultHorizontal(pageCount: 3, selectedPage: 0, theme: .default)
     }
 }
 #endif
